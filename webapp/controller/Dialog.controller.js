514d6e1d73f44418a9606c26e8b8eac5b99f51cc
Marc BOILLOT - Seidor WorkWell <MB@seidorworkwell.local>
2024-02-15 10:22:44 +0100

    feature get LineNum and get the highest LineNum for add item in Order

--- webapp/controller/Dialog.controller.js
+++ webapp/controller/Dialog.controller.js
@@ -18,7 +18,7 @@ sap.ui.define([
     let Models
     let Views
 
-    return BaseController.extend("wwl.controller.Order", {
+    return BaseController.extend("wwl.controller.Dialog", {
         Formatter: Formatter,
 
         onInit: function () {
@@ -39,12 +39,11 @@ sap.ui.define([
         },
 
         onRouteMatch: async function () {
-            const order = await Models.Orders().filter("DocumentStatus eq 'bost_Open'").top(5).get();
-            const order2 = await Models.Orders().top(5).get()
+
             const item = await Models.Items().top(10).get()
             const BusinessPartners = await Models.BusinessPartners().top(10).get()
+            await this.getOrders()
 
-            this._setModel(order.value, "ordersModel")
             this._setModel(item.value, "itemsModel")
             this._setModel(BusinessPartners.value, "BusinessPartnersModel")
 
@@ -61,6 +60,16 @@ sap.ui.define([
             // console.log("itemsInSpecificBinLocation ::", itemsInSpecificBinLocation.value)
         },
 
+        getOrders: async function () {
+            const order = await Models.Orders().filter("DocumentStatus eq 'bost_Open'").top(15).get();
+            this._setModel(order.value, "ordersModel")
+        },
+
+        onCancel: function () {
+            sap.ui.getCore().byId("AddItemToOrder").close()
+        },
+
+
         onMasterView: function () {
             this.getOwnerComponent().getRouter().navTo('Master')
         },
@@ -75,19 +84,19 @@ sap.ui.define([
                 this._oDialogDetail = Fragment.load({
                     name: "wwl.view.Items",
                     controller: this
-                }).then(function (oItems) {
+                }).then(function (oDialogItem) {
+                    that.oView.addDependent(oDialogItem);
+                    oDialogItem.attachAfterClose(() => oDialogItem.destroy())
+                    oDialogItem.getEndButton().attachPress(() => {
+                        oDialogItem.close()
+                    })
 
-                    that.oView.addDependent(oItems);
-                    oItems.attachAfterClose(() => oItems.destroy())
-                    oItems.getEndButton(function () {
-                        oItems.close()
-                    });
                     // je set le selectedRow pour pouvoir mettre a jour le model ordersModel et defini un nom de model pour pouvoir l'appeler dans la vue
                     that._setModel({
                         selectedRow: selectedRow,
                     }, "fragmentModel");
 
-                    oItems.open();
+                    oDialogItem.open();
 
                     const table = that._byId("table")
                     console.log("id table", table)
@@ -98,14 +107,11 @@ sap.ui.define([
                 })
             }
         },
-        onClose: function () {
-            this._byId("itemsDialog").close();
-        },
 
-        createItemDialog: function (oEvent) {
+        onOpenDialogAddItem: function (oEvent) {
             // const selectedRow = oEvent.getSource().getBindingContext("ordersModel").getObject()
             let that = this
-            if (!this._byId("helloPopover")) {
+            if (!this._byId("AddItemToOrder")) {
                 this._oDialogCreate = Fragment.load({
                     name: "wwl.view.AddItemToOrder",
                     controller: this
@@ -116,7 +122,6 @@ sap.ui.define([
                     oNewItems.getEndButton(function () {
                         oNewItems.close()
                     });
-
 
                     oNewItems.setModel(new JSONModel({}), "selectedItemModel2")
                     // that._setModel({
@@ -132,11 +137,52 @@ sap.ui.define([
             }
         },
 
-        handleClose: function () {
-            this._byId("helloPopover").close();
+        onAddItemInOrder: function (event) {
+            let that = this;
+            const dialog = event.getSource().getParent();
+            const selectedItem = dialog.getModel("selectedItemModel2").getData();
+            const idOrder = this._getModel("fragmentModel").getData().selectedRow.DocEntry;
+
+            const documentLines = this._getModel("fragmentModel").getData().selectedRow.DocumentLines;
+
+            if (Array.isArray(documentLines)) {
+                const lineNumArray = documentLines.map(docLine => docLine.LineNum);
+                const highestLineNum = Math.max(...lineNumArray);
+
+                console.log("fragmentmodel : ", idOrder);
+                console.log("selectedItem dans le onPostItem ::", selectedItem);
+                console.log("LineNum Array:", lineNumArray);
+                console.log("Highest LineNum:", highestLineNum);
+
+                const dataToPatch = {
+                    DocumentLines: [
+                        {
+                            LineNum: highestLineNum + 1,
+                            Quantity: selectedItem.Quantity,
+                            ItemCode: selectedItem.ItemCode,
+                        }
+                    ]
+                };
+                // Use the patch method with data and id
+                Models.Orders().patch(dataToPatch, idOrder).then(function () {
+                    console.log("Item added to order successfully");
+                }).catch(function (error) {
+                    console.error("Failed to add item to order", error);
+                });
+
+                that.getOrders();
+                dialog.close();
+            } else {
+                console.error("DocumentLines is not an array");
+            }
         },
 
-        createOrderDialog: function () {
+
+        handleClose: function () {
+            this._byId("AddItemToOrder").close();
+        },
+
+        onOpenDialogCreateOrder: function () {
             let that = this
             if (!this._byId("createOrderDialog")) {
                 this._oDialogCreate = Fragment.load({
@@ -188,92 +234,11 @@ sap.ui.define([
             });
         },
 
-        onPatchOrder: function (oEvent) {
-            let that = this
-
-            let oModel = this.getView().getModel();
-            let selectedLine = oEvent.getSource().getBindingContext("fragmentModel").getObject();
-            console.log("selectedLine", selectedLine);
-
-            if (!this._byId("UpdateItemFragment")) {
-                this._oDialogCreate = Fragment.load({
-                    name: "wwl.view.UpdateItem",
-                    controller: this
-                }).then(function (oNewItems) {
-
-                    that.oView.addDependent(oNewItems);
-                    oNewItems.attachAfterClose(() => oNewItems.destroy())
-                    oNewItems.getEndButton(function () {
-                        oNewItems.close()
-                    });
-                    oNewItems.open();
-                });
-            } else {
-                this._oDialogCreate.then(function (oDialog) {
-                    oDialog.open();
-                })
-            }
-            let aItems = oModel.getProperty("/Items");
-            if (aItems) {
-                let sItemDescription = oModel.getProperty("/ItemDescription");
-                let sPriceAfterVAT = oModel.getProperty("/PriceAfterVAT");
-                let sCurrency = oModel.getProperty("/Currency");
-                let sQuantity = oModel.getProperty("/Quantity");
-                aItems.Items.push({
-                    ItemDescription: sItemDescription,
-                    PriceAfterVAT: sPriceAfterVAT,
-                    Currency: sCurrency,
-                    Quantity: sQuantity
-                });
-
-                // Effectuez la requête PATCH pour mettre à jour l'ordre
-                Models.Orders().patch(aItems).then(function () {
-                    console.log("PATCH successful");
-                }).catch(function (error) {
-                    console.error("PATCH failed", error);
-                });
-            } else {
-                console.error("Item not found");
-            }
-        },
-
-        onSelectChange: function (event) {
-            const selectedItem = event.getSource().getSelectedItem().getBindingContext("itemsModel").getObject()
-            this._getModel("selectedItemModel2").getData().ItemCode = selectedItem.ItemCode
-        },
-
-        onPostItem: function (event) {
-            const dialog = event.getSource().getParent()
-            const selectedItem = dialog.getModel("selectedItemModel2").getData()
-            const idOrder = this._getModel("fragmentModel").getData().selectedRow.DocEntry
-            console.log("fragmentmodel : ", idOrder)
-            console.log("selectedItem dans le onPostItem ::", selectedItem)
-
-
-            const dataToPatch = {
-                DocumentLines: [
-                    {
-                        Quantity: selectedItem.Quantity,
-                        ItemCode: selectedItem.ItemCode,
-                    }
-                ]
-            }
-
-            // j'utilise la methode patch
-            Models.Orders().patch(dataToPatch, idOrder).then(function () {
-                console.log("Item added to order successfully");
-            }).catch(function (error) {
-                console.error("Failed to add item to order", error);
-            });
-           dialog.close()
-
-        },
-
         onOpenDialogDelete: function () {
             let that = this
             if (!this._byId("deleteItem")) {
                 this._oDialogCreate = Fragment.load({
-                    name: "wwl.view.ValidationDelete",
+                    name: "wwl.view.DeleteValidation",
                     controller: this
                 }).then(function (oNewItems) {
 
@@ -291,15 +256,6 @@ sap.ui.define([
             }
         },
 
-        onDeleteItem: function (oEvent) {
-            const selectedRow = oEvent.getSource().getBindingContext("ordersModel").getObject()
-            console.log("selectedRow", selectedRow)
-            let that = this
-
-            console.log("selected row : ", selectedRow);
-
-
-        }
 
     })
 });
