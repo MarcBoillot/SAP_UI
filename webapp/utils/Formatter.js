03e80c5ea6e9baabcc136d0ec2732d9bb6f1f28f
Marc BOILLOT - Seidor WorkWell <MB@seidorworkwell.local>
2024-06-18 11:12:03 +0200

    init

--- webapp/utils/Formatter.js
+++ webapp/utils/Formatter.js
@@ -8,19 +8,5 @@ sap.ui.define([
         formatDate:function (DocDate) {
             return new Date(DocDate).toLocaleDateString('fr')
         },
-        //
-        // formatterMessage: {
-        //     getStatusType: function(sStatus) {
-        //         switch (sStatus) {
-        //             case "delivered":
-        //                 return MessageType.Success;
-        //             case "partially delivered":
-        //                 return MessageType.Warning;
-        //             default:
-        //                 return MessageType.Error;
-        //         }
-        //     }
-        // }
-
     }
 });
